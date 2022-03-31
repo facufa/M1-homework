92035eb95bc94f95e91bb0cb670a29b8e0a6c33f
facufarias <facundof20@hotmail.com>
2022-03-31 09:47:17 -0300

    termine la tarea 2

--- 02-JavaScriptAvanzado-II/homework/homework.js
+++ 02-JavaScriptAvanzado-II/homework/homework.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { cache } = require("@11ty/eleventy/src/TemplateCache");
+
 function counter() {
   // Retorna una funcion que cuando sea invocada retorne un valor creciente.
   // el primer valor deberia ser 1.
@@ -7,7 +9,16 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+
+  let contador = 1;
+    return function() {
+       return contador++;
+  }
 }
+/*
+ const suma = counter(); --- guardamos a la funcion del scope globla en una variable.
+ suma();    ----- asi es comi invocamos a la funcion.
+*/ 
 
 function cacheFunction(cb) {
   // Usa closures para crear un caché para la función cb.
@@ -21,6 +32,17 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+
+  var cache = {}; // usa un objeto donde cada propiedad sea un argumento. aqui guardo los argumentos 
+  return function (argumento) {
+    if(cache.hasOwnProperty(argumento)) {
+      return cache[argumento];
+    } else {
+      cache[argumento] = cb(argumento);
+        return cache[argumento]
+    }
+  }
+
 }
 
 // Bind
@@ -38,11 +60,15 @@ var alumno = {
 function getNombre(){
   return this.nombre;
 }
+
+//getNombre(); undefined porque nombre no esta definido en la el scope global.
+
  // Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que bindear el this!
-let getNombreInstructor = undefined;
-let getNombreAlumno = undefined;
+
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 
 /*Guardar en las siguientes tres variables una función que devuelva una cadena utilizando la función "crearCadena"
@@ -61,11 +87,11 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena){
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que usar bind para "bindear" algunos parámetros de la función crearCadena.
 
-let textoAsteriscos = undefined;
+let textoAsteriscos = crearCadena.bind(this,'*','*');
 
-let textoGuiones = undefined;
+let textoGuiones = crearCadena.bind(this,'-','-');
 
-let textoUnderscore = undefined;
+let textoUnderscore = crearCadena.bind(this,'_','_');
 
 
 
